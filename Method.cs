b1953378839b0a84f521f3f7f9390e6f1a050963
Siya Nkuna <siya.nkuna@vcontractor.co.za>
2024-03-27 22:55:24 +0200

    project start

---
+++ Method.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+namespace LINQ {
+
+  public class Method {
+
+    public string ClassName { get; set; }
+
+    public Method() {
+      ClassName = "Method class";
+    }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ {
+	internal class Program {
+		private static void Main(string[] args) {
+
+			Method method = new();
+
+			Console.WriteLine("In: " + method.ClassName);
+		}
+	}
+}
